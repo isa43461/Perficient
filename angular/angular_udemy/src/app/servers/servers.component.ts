b6299beb612f4dbb39c1f89b7b4050db2e1a05a6
Isabela <isabela.acevedog@perficient.com>
2022-02-14 14:51:33 -0500

    Angular Tutorial-Section3

--- angular/angular_udemy/src/app/servers/servers.component.ts
+++ angular/angular_udemy/src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserver', 'TestServer 2']
   
   constructor() { 
     setTimeout(() => {
@@ -21,7 +23,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event){
