b8a81774d4b9b21c53604c0901342a0b21dad81e
Isabela <isabela.acevedog@perficient.com>
2022-03-02 14:05:26 -0500

    Tutorial_RXJS_1

---
+++ angular/rxjs_tutorial/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rxjs_tutorial';
+  observable : any;
+
+  ngOnInit(){
+    this.observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+  }
+
+  rxjsFun(){
+    console.log('just before subscribe');
+    this.observable.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
+  }
+}
