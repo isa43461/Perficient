9843c19744e320c775128a454b69962f35bc4e12
Isabela <isabela.acevedog@perficient.com>
2022-03-02 17:16:26 -0500

    Tutorial_RXJS_10_2

--- angular/rxjs_tutorial/src/app/app.component.ts
+++ angular/rxjs_tutorial/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { interval, fromEvent} from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { interval, fromEvent, merge, empty} from 'rxjs';
+import { switchMap, scan, takeWhile, startWith, mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +13,20 @@ export class AppComponent {
   constructor(){}
 
   ngOnInit(){
-    fromEvent(document, 'click').pipe(switchMap(() => interval(1000))).subscribe(console.log)
+    const remainingLabel = document.getElementById('remaining');
+    const pauseButton = document.getElementById('pause');
+    const resumeButton = document.getElementById('resume');
+
+    const obsInterval = interval(1000).pipe(mapTo(-1));
+    const pause = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+    const resume = fromEvent(resumeButton, 'click').pipe(mapTo(true));
+
+    const timer = merge(pause, resume).pipe(
+      startWith(true),
+      switchMap(val => (val ? obsInterval : empty())),
+      scan((acc,curr) => (curr ? curr + acc : acc), 10),
+      takeWhile(v => v >= 0)
+    )
+    .subscribe((val:any) => (remainingLabel.innerHTML = val))
   }
 }
