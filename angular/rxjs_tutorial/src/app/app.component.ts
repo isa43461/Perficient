4f0047c108ec504a9d95a07f52fa41eaecfde813
Isabela <isabela.acevedog@perficient.com>
2022-03-02 14:53:41 -0500

    Tutorial_RXJS_6

--- angular/rxjs_tutorial/src/app/app.component.ts
+++ angular/rxjs_tutorial/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { from, fromEvent } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { timer } from 'rxjs';
+import { tap, mapTo, share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,21 @@ export class AppComponent {
   constructor(){}
 
   ngOnInit(){
-    const clicks = fromEvent(document, 'click');
-    const positions = clicks.pipe(tap(ev => console.log('Procesado' + ev)));
-    positions.subscribe(pos => console.log(pos))
+    const time = timer(1000);
+
+    const obs = time.pipe(
+      tap(()=> console.log('TAP ON')),
+      mapTo('End OBS')
+      );
+
+    const subs = obs.subscribe(val => console.log(val));
+    const subs2 = obs.subscribe(val => console.log(val));
+
+    const shareobs = obs.pipe(share());
+
+    console.log('SHARE ON');
+    const subs3 = shareobs.subscribe(val => console.log(val))
+    const subs4 = shareobs.subscribe(val => console.log(val))
+
   }
 }
