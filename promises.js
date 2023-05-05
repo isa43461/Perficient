f35174059ac03ea4f5d93cc4bd0e1edf222bdb58
Isabela Acevedo García <44243406+isa43461@users.noreply.github.com>
2023-05-05 12:06:56 -0500

    Update promises.js

--- promises.js
+++ promises.js
@@ -29,3 +29,39 @@ function allPromise(prom){
 }
 
 allPromise([p1,p2]).then((ej) => console.log(ej)).catch((ej) => console.log(ej))
+
+//punto 2
+
+let p1 = Promise.resolve(33);
+let p2 = new Promise((resolve) => setTimeout(() => resolve(66), 0));
+let p3 = new Promise((resolve, reject) => {
+  reject("reject");
+});
+let p4 =  99;
+
+function allSettlePromise(prom){
+    return new Promise(async (resolve, reject) => {
+        let A = [];
+        if(prom.length === 0){
+            return resolve(prom)
+        }
+        else{
+            for(let i = 0; i < prom.length; i++){
+                if(typeof(prom[i]) === 'object'){
+                    try{
+                       const val = await prom[i];
+                       A.push({status: 'fulfilled', value: val});
+                    }
+                    catch(err){
+                        A.push({status: 'rejected', value: err});
+                    }
+                } else{
+                    A.push({status: 'fulfilled', value: prom[i]});
+                }
+            }
+            resolve(A)
+        }
+    })
+}
+
+allSettlePromise([p1, p2, p4, p3]).then((ej) => console.log(ej));
