5031696c2daf4f9ce143829cb6585eb28e38fd40
Isabela Acevedo García <44243406+isa43461@users.noreply.github.com>
2023-05-05 15:01:18 -0500

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,43 +1,33 @@
-//punto 1
-
-let p1 = Promise.resolve(3);
-let p2 = Promise.resolve(1337);
-let p3 = new Promise((resolve, reject) => {
-  reject("reject");
-});
-function allPromise(prom){
-    return new Promise((resolve, reject) => {
-        let A = [];
-        let rej = [];
-        if(prom.length === 0){
-            return resolve(prom)
-        }
-        else{
-            for(let i = 0; i < prom.length; i++){
-                prom[i].then((value) => A.push(value)).catch((err) => rej.push(err)).finally(() => {
-                    if(i === prom.length - 1){
-                        if(rej.length != 0){
-                            reject(new Error("fallo"))
-                        } else {
-                            resolve(A)
-                        }
-                    }
-                });
-            }
-        }
-    })
-}
-
-allPromise([p1,p2]).then((ej) => console.log(ej)).catch((ej) => console.log(ej))
-
-//punto 2
-
 let p1 = Promise.resolve(33);
 let p2 = new Promise((resolve) => setTimeout(() => resolve(66), 0));
 let p3 = new Promise((resolve, reject) => {
   reject("reject");
 });
-let p4 =  99;
+let p4 =  99; 
+let p5;
+
+function allPromise(prom){
+    return new Promise(async (resolve, reject) => {
+        let A = [];
+        if(prom.length === 0){
+            return resolve(prom)
+        }
+        else{
+            for(let i = 0; i < prom.length; i++){
+                try{
+                   const value = await prom[i];
+                   A.push(value);
+                }
+                catch(err){
+                    reject(new Error('A promise has been rejected!'))
+                }
+            }
+            resolve(A)
+        }
+    })
+}
+
+allPromise([p1, p2, p4]).then((ej) => console.log(ej)).catch((error) => console.log(error));
 
 function allSettlePromise(prom){
     return new Promise(async (resolve, reject) => {
@@ -47,16 +37,12 @@ function allSettlePromise(prom){
         }
         else{
             for(let i = 0; i < prom.length; i++){
-                if(typeof(prom[i]) === 'object'){
-                    try{
-                       const val = await prom[i];
-                       A.push({status: 'fulfilled', value: val});
-                    }
-                    catch(err){
-                        A.push({status: 'rejected', value: err});
-                    }
-                } else{
-                    A.push({status: 'fulfilled', value: prom[i]});
+                try{
+                   const val = await prom[i];
+                   A.push({status: 'fulfilled', value: val});
+                }
+                catch(err){
+                    A.push({status: 'rejected', value: err});
                 }
             }
             resolve(A)
